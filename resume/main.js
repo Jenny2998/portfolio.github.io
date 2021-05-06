3aab92a01f93f7e9347d8dd19db7f67917c2dde3
Jenny2998 <77750512+Jenny2998@users.noreply.github.com>
2021-05-05 20:00:33 -0400

    Add files via upload

--- resume/main.js
+++ resume/main.js
@@ -1,2 +1,16 @@
-const element =  document.querySelector('.social-media')
-element.classList.add('animated', 'shake')
+const onNameClick = function () {
+  //  console.log('got into function')
+  //   $('#message').text('Welcome to the moon.')
+  //   $('#message').css('background-Color', 'blue')
+  //   setTimeout(() => $('#message').text(''), 3000)
+  //   $('.container').addClass('animated rollOut');
+  //   $('#welcomePage').addClass('animated rollIn')
+  }
+
+const addHandlers = () => {
+    // $('#nextView').on('click', onMoonClick)
+  }
+
+  $(() => {
+    addHandlers()
+  })
